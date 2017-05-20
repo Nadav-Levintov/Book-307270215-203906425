4f5476591e3a5656d59962388e8584c8a3e1b06a
Nadav-Levintov <DSP155@Gmail.com>
2017-05-20 15:21:32 +0300

    Kinda tested BookScoreInitializerImpl, need to find a better way...

--- book-test/src/test/java/il/ac/technion/cs/sd/book/test/BookScoreInitializerImplTest.java
+++ book-test/src/test/java/il/ac/technion/cs/sd/book/test/BookScoreInitializerImplTest.java
@@ -1,20 +1,18 @@
 package il.ac.technion.cs.sd.book.test;
 
-import com.google.inject.*;
-import db_utils.DataBase;
-import db_utils.DataBaseFactory;
-import db_utils.DataBaseFactoryImpl;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import db_utils.DataBaseModule;
-import il.ac.technion.cs.sd.book.app.BookScoreInitializer;
 import il.ac.technion.cs.sd.book.app.BookScoreInitializerImpl;
-import il.ac.technion.cs.sd.book.ext.LineStorageModule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
 import java.io.File;
-import java.util.List;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by Nadav on 15-May-17.
@@ -31,9 +29,12 @@ public class BookScoreInitializerImplTest {
 
         String fileContents =
                 new Scanner(new File(BookScoreInitializerImplTest.class.getResource("small.xml").getFile())).useDelimiter("\\Z").next();
-        imp.setup(fileContents);
-
-        /* Assert no exceptions are thrown */
+        try {
+            imp.setup(fileContents);
+        }catch (Exception e)
+        {
+            fail("Should not throw any exceptions");
+        }
 
     }
 
