6601ab3419730cc15da97eb10c6f53b1e5a3b7ed
Nadav-Levintov <dsp155@gmail.com>
2017-05-21 21:19:04 +0300

    fixed BookScoreModule

--- book-test/src/test/java/il/ac/technion/cs/sd/book/test/BookScoreModule.java
+++ book-test/src/test/java/il/ac/technion/cs/sd/book/test/BookScoreModule.java
@@ -1,6 +1,10 @@
 package il.ac.technion.cs.sd.book.test;
 
 import com.google.inject.AbstractModule;
+import db_utils.DataBase;
+import db_utils.DataBaseFactory;
+import db_utils.DataBaseFactoryImpl;
+import db_utils.DataBaseImpl;
 import il.ac.technion.cs.sd.book.app.BookScoreInitializer;
 import il.ac.technion.cs.sd.book.app.BookScoreInitializerImpl;
 import il.ac.technion.cs.sd.book.app.BookScoreReader;
@@ -10,7 +14,10 @@ import il.ac.technion.cs.sd.book.app.BookScoreReaderImpl;
 class    BookScoreModule extends AbstractModule {
   @Override
   protected void configure() {
+    this.bind(DataBaseFactory.class).to(DataBaseFactoryImpl.class);
+    this.bind(DataBase.class).to(DataBaseImpl.class);
     this.bind(BookScoreInitializer.class).to(BookScoreInitializerImpl.class);
     this.bind(BookScoreReader.class).to(BookScoreReaderImpl.class);
+
   }
 }
